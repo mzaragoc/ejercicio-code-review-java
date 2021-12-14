2ce2fc74485608e54b1d798a64c7bad5cac60d46
mzaragoc <96130078+mzaragoc@users.noreply.github.com>
2021-12-14 17:47:53 -0500

    Update Utilitarios.java

--- src/Utilitarios.java
+++ src/Utilitarios.java
@@ -1,4 +1,6 @@
+
 public class Utilitarios {
+    //TODO la clase y el resto de metodos esta en espaniol
     public int average(int params[]) {
         int avg = 0;
         for (int i = 0; i < params.length; i++) {
@@ -6,7 +8,7 @@ public class Utilitarios {
         }
         return avg / params.length;
     }
-
+    
     public double factorial(int numero) {
         double resultado = 1;
         if (numero == 1 || numero == 0) {
@@ -18,13 +20,16 @@ public class Utilitarios {
         }
         return resultado;
     }
-
+    
+    //El parametro deberia llamarse radio
     public double areaCirculo(double r) {
+        //Todo se podria utilizar Math.PI
         final double PI = 22 / 7;
         return PI * (r * r);
     }
 
     //b = base ; h= altura
+    //TODO si se pone los nombre en los parametros no haria falta comentarios
     public double areaTriangulo(double b, double h) {
         double resultado = 0d;
         resultado = (b * h) / 2;
@@ -32,7 +37,9 @@ public class Utilitarios {
     }
 
     public double calculoIva(double valor) {
+        //TODO DEBERIA esta IVA en una clase constante o en 
         final int IVA = 12;
+        //TODO 100 es una constante
         return (valor * IVA) / 100;
     }
 

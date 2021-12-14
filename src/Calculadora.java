b511fa91c80471eee299741ec887c0c220342b8f
mzaragoc <96130078+mzaragoc@users.noreply.github.com>
2021-12-14 17:29:47 -0500

    Update Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,6 +5,7 @@ public class Calculadora {
 
     public int suma(int num1, int num2) {
         //metodo para crear una suma
+        //TODO se deberia hacer el return  de la suma return num1+num2
         int resul = 0;
         resul = num1 + num2;
         return resul;
@@ -12,6 +13,7 @@ public class Calculadora {
 
     public int resta(int num1, int num2) {
         //metodo para crear la resta
+        //TODO se deberia hacer el return  de la suma return num1+num2
         int resul = 0;
         resul = num1 - num2;
         return resul;
@@ -20,6 +22,7 @@ public class Calculadora {
     public int multipli(int num1, int num2) {
         //metodo para crear la multiplicacion
         int resul = 0;
+        //TODO se deberia hacer el return  de la suma return num1+num2
         resul = num1 * num2;
         return resul;
     }
@@ -27,6 +30,7 @@ public class Calculadora {
     public double divi(int num1, int num2) {
         //metodo para la division
         //double es para un valor con punto decimal
+        //Se deberia validar que el num2 sea diferente de 0 para que no de Division por 0 y retornar de una sola
         double resul = 0;
         resul = num1 / num2;
         return resul;
